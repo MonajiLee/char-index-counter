bb13021ea00046a8f17bb28dbf417bc96f9d4142
MonajiLee <lee.smonica@gmail.com>
2018-09-26 20:53:58 +0000

    Changed obj values to array to record indices

--- character-and-indices-counting.js
+++ character-and-indices-counting.js
@@ -3,13 +3,19 @@ function countLetters(string){
     var letters = noSpaces.split("");
     var uniqueCounterObj = {}
     for (var i = 0 ; i < letters.length ; i++){
-            if (uniqueCounterObj[letters[i]] == undefined){
-                uniqueCounterObj[letters[i]] = 1;
-            } else {
-                uniqueCounterObj[letters[i]] += 1;
-            }
+// records the number of repeats     
+        if (uniqueCounterObj[letters[i]] == undefined){
+            uniqueCounterObj[letters[i]] = [i];
+        } else {
+            uniqueCounterObj[letters[i]].push(i);
         }
-        return uniqueCounterObj;
+// record the indices of any repeating letters
+    }
+    return uniqueCounterObj;
 }
 
-console.log(countLetters("sweetie"))
+console.log(countLetters("hello"))
+
+// var uniqueCounterObj = {
+//     l: {[1, [array]},  
+//          indices: [array] i: 2, g: 1, h: 4, t: 2, o: 2, u: 2, s: 2, e: 3, n: 1 }
